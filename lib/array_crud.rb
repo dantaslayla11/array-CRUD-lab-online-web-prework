22eee918b6a8f3b566a79afda16988d4af9c935b
layla dantas <laylatdantas@gmail.com>
2019-05-17 19:50:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,8 +22,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  a = [1, 2, 3, 4]
-  a.shift(1)
+  a = ["wow", "I", "am", "really", "learning arrays!"]
+  a.shift("wow")
 end
 
 def retrieve_element_from_index(array, index_number)
