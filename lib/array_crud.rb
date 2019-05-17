695bdc1f5405b6af8c3cf9d3f6e9310dab4d310d
layla dantas <laylatdantas@gmail.com>
2019-05-17 19:49:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  a = [1, 2, 3, 4]
-  a.pop(4)
+  a = ["wow", "I", "am", "really", "learning arrays!"]
+  a.pop()
 end
 
 def remove_element_from_start_of_array(array)
