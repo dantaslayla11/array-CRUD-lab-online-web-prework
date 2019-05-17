f7d64e3c446c8ac359855d94dea4d24c500c1fe7
layla dantas <laylatdantas@gmail.com>
2019-05-17 19:48:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  a = [2, 3, 4]
-  a.unshift(1)
+  a = ["wow", "I", "am", "really", "learning"]
+  a.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
